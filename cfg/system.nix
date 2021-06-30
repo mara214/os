804b7032c7a36606ab7286fd4af5206946aebd35
Maximilian Schulke <hello@maximilianschulke.com>
2021-06-30 13:49:54 +0200

    Add the base system config

---
+++ cfg/system.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
+  boot.kernelParams = [ "intel_idle.max_cstate=1" "acpi_backlight=vendor" ];
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    device = "nodev";
+    efiSupport = true;
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  time.timeZone = "Europe/Berlin";
+
+  i18n.defaultLocale = "de_DE.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "de";
+  };
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    extraPackages = [ pkgs.vaapiIntel ];
+  };
+
+  virtualisation.docker.enable = true;
+}
