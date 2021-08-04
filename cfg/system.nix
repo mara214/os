be58eedf859be4265f0aeabc6af59507b2a49da7
Maximilian Schulke <hello@maximilianschulke.com>
2021-08-04 08:15:27 +0200

    Update the kernel parameters to fix idling while shutdown

--- cfg/system.nix
+++ cfg/system.nix
@@ -9,18 +9,28 @@
     options = "--delete-older-than 14d";
   };
 
-  boot.kernelParams = [ "intel_idle.max_cstate=1" "acpi_backlight=vendor" ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub = {
-    enable = true;
-    version = 2;
-    device = "nodev";
-    efiSupport = true;
+  boot = {
+    loader = {
+      efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        version = 2;
+        device = "nodev";
+        efiSupport = true;
+      };
+    };
+
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelParams = [
+      "intel_idle.max_cstate=1"
+      "acpi_backlight=vendor"
+      "acpi_osi='!Windows 2013'"
+      "acpi_osi=Linux"
+      "nogpumanager"
+    ];
   };
 
   time.timeZone = "Europe/Berlin";
-
   i18n.defaultLocale = "de_DE.UTF-8";
   console = {
     font = "Lat2-Terminus16";
