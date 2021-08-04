64aa66a107e9f3c516ca8cfc06b4822374d77df5
Maximilian Schulke <hello@maximilianschulke.com>
2021-08-04 08:17:48 +0200

    Add a cron job for background backups

--- cfg/services.nix
+++ cfg/services.nix
@@ -4,6 +4,13 @@
   services.fwupd.enable = true;
   services.openssh.enable = true;
 
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+      "0 12 * * * max /home/max/.zsh/utils/backup now"
+    ];
+  };
+
   systemd.services.backlight-permissions = {
     description = "Fix the backlight permissions";
     serviceConfig = {
