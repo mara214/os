2206015f1d0dc0a9a4705c3ae3e23538bd250c25
Maximilian Schulke <hello@maximilianschulke.com>
2021-09-25 15:19:44 +0200

    Use specializations instead of changing imports and rebuilding

--- configuration.nix
+++ configuration.nix
@@ -1,24 +1,34 @@
 { config, pkgs, ... }:
 
-{
-  imports =
-    [
-      ./hardware-configuration.nix
+let
+  imports = [
+    ./hardware-configuration.nix
 
-      ./cfg/desktop.nix
-      ./cfg/io.nix
-      ./cfg/networking.nix
-      ./cfg/packages.nix
-      ./cfg/services.nix
-      ./cfg/system.nix
-      ./cfg/user.nix
-      ./cfg/gfx/intel.nix
+    ./cfg/desktop.nix
+    ./cfg/io.nix
+    ./cfg/networking.nix
+    ./cfg/packages.nix
+    ./cfg/services.nix
+    ./cfg/system.nix
+    ./cfg/user.nix
 
-      ./env/home.nix
-      ./env/hum-systems.nix
-      ./env/bielefeld.nix
-      ./env/travel.nix
-    ];
+    ./env/home.nix
+    ./env/hum-systems.nix
+    ./env/bielefeld.nix
+    ./env/travel.nix
+  ];
+in {
+  imports = imports ++ [ ./cfg/gfx/intel.nix ];
+
+  specialisation = {
+    egpu = {
+      inheritParentConfig = false;
+      configuration =
+        {
+          imports = imports ++ [ ./cfg/gfx/nvidia.nix ];
+        };
+    };
+  };
 
   # Determines NixOS release.
   # Don't change this before reading the docs.
