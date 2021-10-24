5075264c9702a6d90328942b96654687b5700944
Maximilian Schulke <hello@maximilianschulke.com>
2021-10-24 09:31:43 +0200

    Add a university environment

--- configuration.nix
+++ configuration.nix
@@ -12,10 +12,11 @@ let
     ./cfg/system.nix
     ./cfg/user.nix
 
+    ./env/bielefeld.nix
     ./env/home.nix
     ./env/hum-systems.nix
-    ./env/bielefeld.nix
     ./env/travel.nix
+    ./env/university.nix
   ];
 in {
   imports = imports ++ [ ./cfg/gfx/intel.nix ];
