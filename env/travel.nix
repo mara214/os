546ecb6fe9b54361b91c4c680299daf5a301ca21
Mara Schulke <mara@schulke.xyz>
2022-05-24 14:01:29 +0200

    Update psk of iphone wifi

--- env/travel.nix
+++ env/travel.nix
@@ -3,6 +3,6 @@
 {
   networking.wireless.networks."iPhone von Mara" = {
     priority = 5;
-    pskRaw = "cd136d9abb9367a66d47cff4add0cf4138e10ebc924634534c821c76e1e8712d";
+    pskRaw = "df1d85621f9119d7258e8c517a6cae382a08cb916d6e855ca3487bb7ec5b1d3b";
   };
 }
