07c1ad2ed5f06d95bb6f2fde8aaf8e89d4deb774
Mara Schulke <mara@schulke.xyz>
2022-07-09 13:29:52 +0200

    Remove unused services

--- os/services.nix
+++ os/services.nix
@@ -3,24 +3,4 @@
 {
   services.fwupd.enable = true;
   services.openssh.enable = true;
-
-  services.cron = {
-    enable = true;
-    mailto = "mara@schulke.xyz";
-    systemCronJobs = [
-      "0 12 * * *  max  DROPBOX_CLI='/home/mara/.local/bin/dropbox-sh' /home/mara/.zsh/utils/backup now"
-    ];
-  };
-
-  services.ssmtp = {
-    enable = true;
-    root = "mara@schulke.xyz";
-    settings = {
-      rewriteDomain = "schulke.xyz";
-    };
-    hostName = "smtp.gmail.com:465";
-    authUser = "schulkemaximilian@gmail.com";
-    authPassFile = "/etc/nixos/secrets/mail/pass";
-    useTLS = true;
-  };
 }
